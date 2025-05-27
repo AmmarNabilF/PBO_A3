e3430646a2a2a680830feb71ab0fc0104743319b
Faiz-Awr <faizulanwarwandi@gmail.com>
2025-05-27 10:11:14 +0800

    New abstract class "Barang" with Pasokan Class as subclass

---
+++ PA_K3/src/main/java/com/model/Barang.java
@@ -0,0 +1,32 @@
+package com.model;
+
+public abstract class Barang {
+    protected String id;
+    protected String nama;
+    protected int stok;
+
+    public Barang(String id, String nama, int stok) {
+        this.id = id;
+        this.nama = nama;
+        this.stok = stok;
+    }
+
+    public String getId() { return id; }
+    public String getNama() { return nama; }
+    public int getStok() { return stok; }
+    public void setStok(int stok) { this.stok = stok; }
+    public void setNama(String nama) { this.nama = nama; }
+    public void setId(String id) { this.id = id; }
+
+    // Method abstract, wajib diisi oleh subclass
+    public abstract void tampilkanInfo();
+
+    @Override
+    public String toString() {
+        return "Barang{" +
+                "id='" + id + '\'' +
+                ", nama='" + nama + '\'' +
+                ", stok=" + stok +
+                '}';
+    }
+}
--- PA_K3/src/main/java/com/model/Pasokan.java
+++ PA_K3/src/main/java/com/model/Pasokan.java
@@ -1,55 +1,21 @@
 package com.model;
 
-public class Pasokan {
-    private String idPasokan;
+public class Pasokan extends Barang {
     private String idPemasok;
-    private String namaBahan;
     private double hargaSatuan;
-    private int stok;
 
     public Pasokan(String idPasokan, String idPemasok, String namaBahan, double hargaSatuan, int stok) {
-        this.idPasokan = idPasokan;
+        super(idPasokan, namaBahan, stok);
         this.idPemasok = idPemasok;
-        this.namaBahan = namaBahan;
-        this.hargaSatuan = hargaSatuan;
-        this.stok = stok;
-    }
-
-    public String getIdPasokan() {
-        return idPasokan;
-    }
-
-    public String getIdPemasok() {
-        return idPemasok;
-    }
-
-    public String getNamaBahan() {
-        return namaBahan;
-    }
-
-    public double getHargaSatuan() {
-        return hargaSatuan;
-    }
-
-    public int getStok() {
-        return stok;
-    }
-
-    public void setNamaBahan(String namaBahan) {
-        this.namaBahan = namaBahan;
-    }
-
-    public void setHargaSatuan(double hargaSatuan) {
         this.hargaSatuan = hargaSatuan;
     }
 
-    public void setStok(int stok) {
-        this.stok = stok;
-    }
+    public String getIdPemasok() { return idPemasok; }
+    public double getHargaSatuan() { return hargaSatuan; }
+    public void setHargaSatuan(double hargaSatuan) { this.hargaSatuan = hargaSatuan; }
 
     @Override
-    public String toString() {
-        return String.format("%-12s | %-25s | Rp%-13.2f | %-5d", 
-                            idPasokan, namaBahan, hargaSatuan, stok);
+    public void tampilkanInfo() {
+        System.out.println("Pasokan: " + nama + ", Stok: " + stok + ", Harga: " + hargaSatuan);
     }
 }
