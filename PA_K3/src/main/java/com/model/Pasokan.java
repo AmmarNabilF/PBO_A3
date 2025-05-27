60c29bde67224219c79754a8a0984404b4a4ef47
Faiz-Awr <faizulanwarwandi@gmail.com>
2025-05-27 10:33:33 +0800

    perbaikan tampilan pasokan

--- PA_K3/src/main/java/com/model/Pasokan.java
+++ PA_K3/src/main/java/com/model/Pasokan.java
@@ -14,8 +14,15 @@ public class Pasokan extends Barang {
     public double getHargaSatuan() { return hargaSatuan; }
     public void setHargaSatuan(double hargaSatuan) { this.hargaSatuan = hargaSatuan; }
 
-    @Override
-    public void tampilkanInfo() {
-        System.out.println("Pasokan: " + nama + ", Stok: " + stok + ", Harga: " + hargaSatuan);
-    }
+@Override
+public void tampilkanInfo() {
+    System.out.println(String.format("%-12s | %-25s | Rp%-13.2f | %-5d", 
+        getId(), getNama(), getHargaSatuan(), getStok()));
+}
+
+@Override
+public String toString() {
+    return String.format("%-12s | %-25s | Rp%-13.2f | %-5d", 
+        getId(), getNama(), getHargaSatuan(), getStok());
+}
 }
